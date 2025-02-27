6590fd034322111fa34c76bad8b04fe59d3e5763
App Generator <51070104+app-generator@users.noreply.github.com>
2025-02-27 12:12:57 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Dashboard(models.Model):
+
+    #__Dashboard_FIELDS__
+    kaufdatum = models.TextField(max_length=255, null=True, blank=True)
+    kaufpreis = models.CharField(max_length=255, null=True, blank=True)
+    verkaufsdatum = models.TextField(max_length=255, null=True, blank=True)
+    verkaufspreis = models.CharField(max_length=255, null=True, blank=True)
+    info = models.TextField(max_length=255, null=True, blank=True)
+
+    #__Dashboard_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Dashboard")
+        verbose_name_plural = _("Dashboard")
+
+
+
+#__MODELS__END
